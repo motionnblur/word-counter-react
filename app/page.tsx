21a1ab4fe1394269d524a2cb04af08f48854ca04
motionnblur <carbonfiberdev@gmail.com>
2024-10-24 21:20:10 +0300

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
 export default function Home() {
   return (
     <main className="w-full h-screen p-5">
-      <div className="w-full h-full flex items-center justify-center flex-col">
-        <div className="w-full h-full">
-          <textarea
-            className="resize-none text-slate-950"
-            rows={10}
-            cols={50}
-          />
-        </div>
-        <div className="w-full h-full">
-          <div className="w-14 h-7 bg-slate-50 rounded text-black cursor-pointer">
-            Count
-          </div>
+      <div className="w-full h-full flex items-center flex-col">
+        <textarea className="resize-none text-slate-950" rows={10} cols={50} />
+        <div className="w-14 h-7 bg-slate-50 rounded text-black cursor-pointer mt-4">
+          Count
         </div>
       </div>
     </main>
