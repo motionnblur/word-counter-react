1b5cb864f42b65dbf793bbde857c211073c07539
motionnblur <carbonfiberdev@gmail.com>
2024-10-24 21:25:49 +0300

    Add count text area

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ export default function Home() {
     <main className="w-full h-screen p-5">
       <div className="w-full h-full flex items-center flex-col">
         <textarea className="resize-none text-slate-950" rows={10} cols={50} />
+        <textarea
+          className="resize-none text-slate-950 mt-3"
+          rows={1}
+          cols={11}
+          placeholder="  Paste a word"
+        />
         <div className="w-14 h-7 bg-slate-50 rounded text-black cursor-pointer mt-4">
           Count
         </div>
