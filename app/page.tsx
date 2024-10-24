ca9401f4d203a1fbd3dc97ef0c27e6b727e6f8ef
motionnblur <carbonfiberdev@gmail.com>
2024-10-24 20:48:45 +0300

    Add text area template

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main className="w-full h-screen"> </main>;
+  return (
+    <main className="w-full h-screen">
+      <textarea className="resize-none text-slate-950" rows={10} cols={50} />
+    </main>
+  );
 }
