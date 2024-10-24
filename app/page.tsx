7340af076ce82d833bfbaced613e7e0ad772fa07
motionnblur <carbonfiberdev@gmail.com>
2024-10-24 20:54:28 +0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <main className="w-full h-screen">
-      <textarea className="resize-none text-slate-950" rows={10} cols={50} />
+    <main className="w-full h-screen p-5">
+      <div className="w-full h-full flex items-center justify-center flex-col">
+        <div className="w-full h-full">
+          <textarea
+            className="resize-none text-slate-950"
+            rows={10}
+            cols={50}
+          />
+        </div>
+        <div className="w-full h-full"></div>
+      </div>
     </main>
   );
 }
