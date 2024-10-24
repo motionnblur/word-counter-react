6691aad085a9e4d70d5dbc64ef7317f5b42f7c99
motionnblur <carbonfiberdev@gmail.com>
2024-10-24 23:10:38 +0300

    First mvp

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
+const searchWord = (
+  textArea: HTMLTextAreaElement,
+  wordToSearch: string
+): number => {
+  const text = textArea.value;
+  const words = text.split(" ");
+  let count = 0;
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === wordToSearch) {
+      count++;
+    }
+  }
+  return count;
+};
 
 export default function Home() {
+  const [wordCount, setWordCount] = useState(0);
   const mainTextAreaRef = useRef<HTMLTextAreaElement>(null);
   const wordToSearchTextAreaRef = useRef<HTMLTextAreaElement>(null);
 
@@ -10,13 +26,13 @@ export default function Home() {
     <main className="w-full h-screen p-5">
       <div className="w-full h-full flex items-center flex-col">
         <textarea
-          className="resize-none text-slate-950"
+          className="resize-none text-slate-950 outline-none"
           rows={10}
           cols={50}
           ref={mainTextAreaRef}
         />
         <textarea
-          className="resize-none text-slate-950 mt-3"
+          className="resize-none text-slate-950 mt-3 outline-none"
           rows={1}
           cols={11}
           placeholder="  Paste a word"
@@ -25,12 +41,16 @@ export default function Home() {
         <div
           className="w-14 h-7 bg-slate-50 rounded text-black cursor-pointer mt-4"
           onMouseDown={() => {
-            console.log(wordToSearchTextAreaRef.current?.value);
+            const wordCount = searchWord(
+              mainTextAreaRef.current!,
+              wordToSearchTextAreaRef.current!.value
+            );
+            setWordCount(wordCount);
           }}
         >
           Count
         </div>
-        <div className="mt-8 text-3xl">0</div>
+        <div className="mt-8 text-3xl">{wordCount}</div>
       </div>
     </main>
   );
