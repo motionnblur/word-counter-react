ae23016d0105525c7536717967ea324488ff267a
motionnblur <carbonfiberdev@gmail.com>
2024-10-24 22:59:21 +0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,33 @@
+"use client";
+
+import { useRef } from "react";
+
 export default function Home() {
+  const mainTextAreaRef = useRef<HTMLTextAreaElement>(null);
+  const wordToSearchTextAreaRef = useRef<HTMLTextAreaElement>(null);
+
   return (
     <main className="w-full h-screen p-5">
       <div className="w-full h-full flex items-center flex-col">
-        <textarea className="resize-none text-slate-950" rows={10} cols={50} />
+        <textarea
+          className="resize-none text-slate-950"
+          rows={10}
+          cols={50}
+          ref={mainTextAreaRef}
+        />
         <textarea
           className="resize-none text-slate-950 mt-3"
           rows={1}
           cols={11}
           placeholder="  Paste a word"
+          ref={wordToSearchTextAreaRef}
         />
-        <div className="w-14 h-7 bg-slate-50 rounded text-black cursor-pointer mt-4">
+        <div
+          className="w-14 h-7 bg-slate-50 rounded text-black cursor-pointer mt-4"
+          onMouseDown={() => {
+            console.log(wordToSearchTextAreaRef.current?.value);
+          }}
+        >
           Count
         </div>
         <div className="mt-8 text-3xl">0</div>
